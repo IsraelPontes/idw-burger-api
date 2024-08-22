989cf3958bf9545db4b3df0b86c9e9bae3e6b666
IsraelPontes <israelcpontes@hotmail.com>
2024-08-22 09:46:28 -0300

    Creating product controller

---
+++ src/app/controlllers/ProductController.js
@@ -0,0 +1,19 @@
+import * as Yup from 'yup'
+
+class ProductController {
+    async store(request, response) {
+        const schema = Yup.object({
+            name: Yup.string().required(),
+            price: Yup.number().required(),
+            category: Yup.string().required(),
+        })
+        try {
+            schema.validate.Sync(request.body, { abortEarly: false })
+        } catch (err) {
+            return response.status(400).json({ error: err.errors })
+        }
+
+        return response.status(201).json({ message: "ok" })
+    }
+}
+export default new ProductController()
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import UserController from "./app/controlllers/UserController";
 import SessionController from "./app/controlllers/SessionController";
+import ProductController from "./app/controlllers/ProductController";
 
 const routes = new Router()
 
 routes.post('/users', UserController.store)
 routes.post('/session', SessionController.store)
+routes.post('/products', ProductController.store)
 
 export default routes
