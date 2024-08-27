b37c92cce8af707cc6931f18c260fe3416cf84ee
IsraelPontes <israelcpontes@hotmail.com>
2024-08-27 11:03:28 -0300

    JWT configuration part 2

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,15 @@ import SessionController from "./app/controlllers/SessionController";
 import ProductController from "./app/controlllers/ProductController";
 import multer from "multer";
 import multerConfig from './config/multer';
+import authMiddleware from "./middleware/auth";
 
 const routes = new Router()
 const upload = multer(multerConfig)
 
 routes.post('/users', UserController.store)
 routes.post('/session', SessionController.store)
+
+routes.use(authMiddleware)
 routes.post('/products', upload.single('file'),ProductController.store)
 routes.get('/products', ProductController.index)
 
